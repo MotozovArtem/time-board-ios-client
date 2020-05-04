14b0c1528847ca36f020f1d6cdbb96e42b005e1c
Tianid <tianid73@gmail.com>
2020-05-04 21:04:48 +0300

    TB-35 Added avatar and some labels Added protocols for ProfileView

---
+++ time-board/Protocols/ProfilePresenterProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  ProfilePresenterProtocol.swift
+//  time-board
+//
+//  Created by Tianid on 04.05.2020.
+//  Copyright © 2020 Tianid. All rights reserved.
+//
+
+import Foundation
---
+++ time-board/Protocols/ProfileViewControllerProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileViewControllerProtocol.swift
+//  time-board
+//
+//  Created by Tianid on 04.05.2020.
+//  Copyright © 2020 Tianid. All rights reserved.
+//
+
+import Foundation
